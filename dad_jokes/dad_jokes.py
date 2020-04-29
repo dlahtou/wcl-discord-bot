b146760ad0824b7220b795a151e09619950d9b12
Kin Fong <bradley.kin.fong@gmail.com>
2020-04-28 23:28:21 -0500

    add dad_jokes.py

---
+++ dad_jokes/dad_jokes.py
@@ -0,0 +1,16 @@
+import requests
+import urllib
+
+ICANHAZDADJOKE_API_PATH = 'https://icanhazdadjoke.com/'
+
+def get_dad_joke():
+    r = requests.get(
+        ICANHAZDADJOKE_API_PATH,
+        headers={'Accept': 'text/plain'},
+        )
+    if r.status_code == 200:
+        return r
+    else:
+        return "Error retrieving dad joke, " + r.status_code
+
+get_dad_joke()
